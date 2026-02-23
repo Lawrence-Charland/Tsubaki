8f969c071594e9230929074edec10f25c0a559e2
Lawrence <1470595583@qq.com>
2026-02-23 13:34:32 +0800

    Fix compilation command and add comments
    Removed redundant 'sudo' from the compilation command and added comments in both Chinese and English.

--- tsubaki.build.sh
+++ tsubaki.build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo sudo g++ -std=c++17 -O3 ./Tsubaki.Qt_creator.d/main.cpp -o /usr/bin/tsubaki -lssl -lcrypto -lpthread
+sudo g++ -std=c++17 -O3 ./Tsubaki.Qt_creator.d/main.cpp -o /usr/bin/tsubaki -lssl -lcrypto -lpthread
+#这里列出了建议的编译命令。当然你可以自己选择其他的参数，或者换用其他的编译器。
+#The suggested compilation commands are listed here. Of course, you can choose other parameters, or change to other compilers by yourself.
